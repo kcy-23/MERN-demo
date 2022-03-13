e10720e80644c0e064be3c2045081d843c77ee79
CY <69207419+kcy-23@users.noreply.github.com>
2022-03-13 21:42:22 +0800

    Added controller & connected routes to controller functions

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,29 @@
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
+const getGoals = (req, res) => {
+  res.status(200).json({ Message: "Get goals" });
+};
+
+// @desc    Set goals
+// @route   POST /api/goals
+// @access  Private
+const setGoals = (req, res) => {
+  res.status(200).json({ Message: "Set goals" });
+};
+
+// @desc    Update goals
+// @route   PUT /api/goals/:id
+// @access  Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ Message: `Update goal ${req.params.id}` });
+};
+
+// @desc    Delete goals
+// @route   DELETE /api/goals/:id
+// @access  Private
+const deleteGoals = (req, res) => {
+  res.status(200).json({ Message: `Delete goal ${req.params.id}` });
+};
+
+module.exports = { getGoals, setGoals, updateGoal, deleteGoals };
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getGoals,
+  setGoals,
+  updateGoal,
+  deleteGoals,
+} = require("../controllers/goalController");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get goals" });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Set goals" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
-});
+router.route("/").get(getGoals).post(setGoals);
+router.route("/:id").put(updateGoal).delete(deleteGoals);
 
 module.exports = router;
